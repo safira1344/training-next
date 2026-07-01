a6264f712d83cad690bab79a3a6743f75048885c
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-06-30 23:13:52 -0300

    feat: add metadata for SEO and Open Graph support in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,26 @@
 import "./globals.css";
 
 import {Header} from '../components/header'
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Home - Next JS!',
+    description: 'Treinando Next JS!',
+    openGraph:{
+        title: 'Treinando Next JS!',
+        description: 'Estou adorando Next JS e sua praticidade !',
+        images: ['public/setup.webp'],
+    },
+    robots: {
+        index: true,
+        follow: true,
+        nocache: true,
+        googleBot: {
+            index: true,
+            follow: true,
+        }
+    }
+}
 
 export default function RootLayout({
     children,
