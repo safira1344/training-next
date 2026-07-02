d3c412f6f1f930e834618fd4e7bb3cc6491f967f
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-07-01 23:56:57 -0300

    feat: implement name change functionality in Button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,7 +1,19 @@
+'use client'
+
+import { useState } from "react"
+
 export function Button() {
+    const [nome, setNome] = useState('Programadora')
+
+    function handleChangeName() {
+        setNome('Fernanda Mirely')
+    }
+
+
     return (
         <div>
-            <button>Alterar nome</button>
+            <button onClick={handleChangeName}>Alterar nome</button>
+            <h3>Nome: {nome}</h3>
         </div>
     )
 }
