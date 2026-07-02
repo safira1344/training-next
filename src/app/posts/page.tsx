e597c17c123155094af8940ca31540c5af91ebe3
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-07-01 23:00:32 -0300

    feat: add PostsPage server component and PageTeste client component for post fetching

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,34 @@
-export default function PostsPage() {
+interface PostProps{
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+}
+
+interface ResponseProps {
+    posts: PostProps[]
+}
+
+// SERVER COMPONENTS - renderizado pelo lado do server
+export default async function PostsPage() {
+
+    const response = await fetch('https://dummyjson.com/posts')
+    const data: ResponseProps = await response.json()
+
+    console.log(data);
+
     return (
         <div>
-            <h1>Todos os posts.</h1>
+            <h1 className="text-center mt-5 mb-2 font-bold text-3xl">Todos os posts.</h1>
+
+            <div className="flex flex-col gap-4 mx-2">
+                {data.posts.map(post => (
+                    <div className="bg-gray-200 p-4 rounded-md" key={post.id}>
+                        <h2 className="font-bold">{post.title}</h2>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
