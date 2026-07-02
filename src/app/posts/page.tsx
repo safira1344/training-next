a2c8bf170cc856b19db456b05fd2660a51490b1c
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-07-02 18:00:01 -0300

    feat: enhance PostsPage with user detail links and clean up fetch functions

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/src/components/button";
+import Link from "next/link";
 
-interface PostProps{
+export interface PostProps{
     id: number;
     title: string;
     body: string;
@@ -18,11 +19,24 @@ export default async function PostsPage() {
     const data: ResponseProps = await response.json()
 
     // é possível fazer com que uma função tenha a diretiva para usar server ou client
-    // async function handleFetchPosts() {
-    //     'use server'
+    async function handleFetchPosts() {
+        'use server'
+        const response = await fetch('https://dummy.json.com/posts')
+        const data: ResponseProps = await response.json()
 
-    //     console.log('CLICOUUUUU')
-    // }
+        console.log(data.posts)
+    }
+
+    async function handleSearchUsers(formData: FormData) {
+        'use server'
+
+        const userId = formData.get('userId')
+
+        const response = await fetch(`https://dummyjson.com/posts/user/${userId}`)
+        const data: ResponseProps = await response.json()
+
+        console.log(data)
+    }
 
     return (
         <div>
@@ -30,15 +44,32 @@ export default async function PostsPage() {
 
             <Button></Button>
 
-            {/* <button onClick={handleFetchPosts}>
+            <button onClick={handleFetchPosts}>
                 Buscar posts
-            </button> */}
+            </button>
+
+            <form 
+            className="flex gap-2 my-4"
+            action={handleSearchUsers}>
+                <input 
+                type="text"
+                placeholder="ID do usuário"
+                className="border border-gray-200 p-2" />
+                <button 
+                className="bg-blue-500 text-white p-2"
+                type="submit">
+                    Buscar usuário
+                </button>
+            </form>
 
             <div className="flex flex-col gap-4 mx-2">
                 {data.posts.map(post => (
                     <div className="bg-gray-200 p-4 rounded-md" key={post.id}>
                         <h2 className="font-bold">{post.title}</h2>
                         <p>{post.body}</p>
+                        <Link className="text-blue-500" href={`/posts/${post.id}`}>
+                        Acessar detalhes
+                        </Link>
                     </div>
                 ))}
             </div>
