a9cf37a35bd2e67f7e526ee9ec6d8762d37b5853
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-07-02 15:36:45 -0300

    refactor: clean up commented code in PostsPage component

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -17,13 +17,22 @@ export default async function PostsPage() {
     const response = await fetch('https://dummyjson.com/posts')
     const data: ResponseProps = await response.json()
 
-    console.log(data);
+    // é possível fazer com que uma função tenha a diretiva para usar server ou client
+    // async function handleFetchPosts() {
+    //     'use server'
+
+    //     console.log('CLICOUUUUU')
+    // }
 
     return (
         <div>
             <h1 className="text-center mt-5 mb-2 font-bold text-3xl">Todos os posts.</h1>
 
             <Button></Button>
+
+            {/* <button onClick={handleFetchPosts}>
+                Buscar posts
+            </button> */}
 
             <div className="flex flex-col gap-4 mx-2">
                 {data.posts.map(post => (
