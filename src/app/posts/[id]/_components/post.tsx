814b27f7caf31c8e7005df0f0e0a10d37a485d9f
Fernanda Mirely <safiraaabarbosaaa@gmail.com>
2026-07-02 22:25:31 -0300

    feat: add PostInfo component for fetching and displaying post details

---
+++ src/app/posts/[id]/_components/post.tsx
@@ -0,0 +1,16 @@
+import { PostProps } from "../../page"
+
+export async function PostInfo({ id }: {id:string}) {
+
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    const response = await fetch(`https://dummyjson.com/posts/${id}`)
+    const data: PostProps = await response.json()
+
+    return (
+        <div>
+            <h2>{data.title}</h2>
+            <p>{data.body}</p>
+        </div>
+    )
+}
